23d46d842f6c7bde5402fe3eb45ff2ff8b7a9495
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-01 23:28:34 +0530

    Create FunctionsAdd.java

---
+++ Day 6/FunctionsAdd.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class FunctionsAdd {
+  
+  public static int sum(int a , int b){
+    int sum = a+b;
+    return sum;
+  }
+
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter 1st number : ");
+      int a = sc.nextInt();
+
+      System.out.println("Enter 2nd number : ");
+      int b = sc.nextInt();
+
+     sum(a,b);   //This is not printed 
+      System.err.println("The sum of two numbers is : " + sum(a,b));
+  }
+}

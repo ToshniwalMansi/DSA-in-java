3cb9cfe44d53ddd39765b6e1a8ffb78053e8f9bd
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-01 23:49:12 +0530

    Create FunctionsFactorial.java

---
+++ Day 6/FunctionsFactorial.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class FunctionFactorial {
+  
+  public static void factorial(int a){
+    
+    if(a>0){
+      int result =1;
+     for(int i = a ; i>= 1 ; i--){
+      result = result*i;
+      }
+      System.out.println("The factorial is : " + result);
+    }
+    else if(a==0){
+      
+      System.out.println("the factorial is : " + 1);
+    }
+    else if(a<0){
+      System.out.println("factorial cant be found for this number ");
+    }
+    
+  }
+
+  public static void main(String[] args) {
+      
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number : ");
+    int a = sc.nextInt();
+
+    factorial(a);
+  }
+}

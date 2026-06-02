aae38c9e04232bb6646ec87917436dc2164707e2
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-02 20:56:42 +0530

    Create AverageFunction.java

---
+++ Day 7/AverageFunction.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class AverageFunction {
+  
+  public static void average(int a , int b , int c){
+    int average = (a+b+c)/3;
+    System.out.println("The average of three numbers is : " + average);
+  }
+
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter 1st number : ");
+      int a = sc.nextInt();
+
+      System.out.print("Enter 2nd number : ");
+      int b = sc.nextInt();
+
+      System.out.print("Enter 3nd number : ");
+      int c = sc.nextInt();
+
+      sc.close();
+
+      average(a,b,c);
+
+  }
+}

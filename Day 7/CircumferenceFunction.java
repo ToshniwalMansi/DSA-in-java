3de967e34bf8e89bcd2fed974b0b42b7031ab395
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-02 21:28:03 +0530

    Create CircumferenceFunction.java

---
+++ Day 7/CircumferenceFunction.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class CircumferenceFunction {
+  
+  public static void circumference(float a){
+    double circumference ;
+    circumference = 2*3.14*a;
+
+    System.out.println("The circumference for the circle is : " + circumference);
+  }
+
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      System.out.print("Enter the radius of circle : ");
+      float a = sc.nextFloat();
+
+      sc.close();
+
+      circumference(a);
+  }
+}

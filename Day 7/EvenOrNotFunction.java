dee8bf2c780b80db2472db12ff9e01e03234ed95
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-02 20:40:08 +0530

    Create EvenOrNotFunction.java

---
+++ Day 7/EvenOrNotFunction.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class EvenOrNotFunctions {
+  public static void evenOrOdd(int a){
+    if(a%2 == 0){
+      System.out.println("The number is even ");
+    }
+    else{
+      System.out.println("The number id odd ");
+    }
+    
+  }
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the number : ");
+      int a = sc.nextInt();
+
+      sc.close();
+
+      evenOrOdd(a);
+  }
+}

a6b26da50968315c83706a6d8c0bf82b4cef50ea
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-02 21:12:05 +0530

    Create GreaterOfTwo.java

---
+++ Day 7/GreaterOfTwo.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class GreaterOfTwoFunction {
+  
+  public static void greaterOfTwo(int a , int b){
+    if(a>b){
+      System.out.println("The greater of two numbers is : " + a);
+    }
+    else if(a<b){
+      System.out.println("The greater of two numbers is : " + b);
+    }
+    return;
+  }
+
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      System.out.print("Enter the 1st number : ");
+      int a = sc.nextInt();
+
+      System.out.print("Enter the 2nd number : ");
+      int b = sc.nextInt();
+
+      sc.close();
+
+      greaterOfTwo(a,b);
+  }
+}

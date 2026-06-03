af1432aab11ed265c7b19079eb287e987f15c4bb
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-03 17:43:46 +0530

    Create InfiniteDoWhileLoop.java

---
+++ Day 8/InfiniteDoWhileLoop.java
@@ -0,0 +1,25 @@
+// Write an ininite loop using do while condition 
+
+import java.util.*;
+public class InfiniteDoWhileLoop {
+  
+
+  public static void infinteLoop(int a){
+    do{
+      System.out.println("This is loop ");
+      a--;
+    }
+    while(a!=a+1);
+    return ;
+  }
+
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Give a number : ");
+      int a = sc.nextInt();
+
+      sc.close();
+
+      infinteLoop(a);
+  }
+}

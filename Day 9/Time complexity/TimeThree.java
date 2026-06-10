bc13da8186da16076151df03b23338d8b4d11b5c
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-10 23:37:27 +0530

    Add time complexity comments to TimeThree.java
    Added comments explaining time complexity of the loops.

---
+++ Day 9/Time complexity/TimeThree.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class TimeThree {
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+
+      for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<m ; j++){
+          System.out.println("hello");
+        }
+      }
+  }
+}
+
+
+
+// outer loop runs n time 
+// inner loop is m time 
+// time compexity is m+n

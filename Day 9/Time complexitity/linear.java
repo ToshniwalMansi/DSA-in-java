9154074dcfef7a78c020a2b348c8e263ebd17f66
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-10 23:25:52 +0530

    Correct loop increment in linear.java
    Fix the loop condition to increment the counter correctly.

---
+++ Day 9/Time complexitity/linear.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class linear{
+  public static void main(String args[]){
+    Scanner sc = new Scanner (System.in);
+    int n = sc.nextInt();
+    for(int i=0 ; i<n ; n+++){
+     System.out.println("hello");
+    }
+  }
+}
+
+
+//Time complexity is directly proportional to input n 

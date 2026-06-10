1e4dfd2ae19c9229f67e786d80968fa2600cc6dd
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-10 23:44:40 +0530

    Create SpaceOne.java

---
+++ Day 9/Space complexity/SpaceOne.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class SpcaeOne {
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+
+      for(int i=0 ; i<n ; i++){
+        System.out.println("hello");
+      }
+  }
+}
+
+
+
+// here scanner will take spcae in memory 
+// int i and n will also take space in memory 
+// this is space complexity

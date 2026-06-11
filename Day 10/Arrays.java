da437cc19d3cf450e5ecf7d47a011716d749a68f
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-11 23:21:53 +0530

    Implement loop to print all array elements
    Added a loop to print all marks in the array.

--- Day 10/Arrays.java
+++ Day 10/Arrays.java
@@ -9,5 +9,11 @@ public class Arrays {
       System.out.println(marks[0]);
       System.out.println(marks[1]);
       System.out.println(marks[2]);
+
+
+    // we can also print all those in loop
+    for(int i=0 ; i<3 ; i++){
+      System.out.println(marks[i]);
+    }
   }
 }

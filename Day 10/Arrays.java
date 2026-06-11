1c635d7ab5fdad3de9be59e65fcb6866849b3d1c
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-11 23:20:10 +0530

    Add Arrays.java to demonstrate array usage

---
+++ Day 10/Arrays.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Arrays {
+  public static void main(String[] args) {
+      int[] marks = new int[3];
+      marks[0] = 99;
+      marks[1] = 89;
+      marks[2] = 90;
+
+      System.out.println(marks[0]);
+      System.out.println(marks[1]);
+      System.out.println(marks[2]);
+  }
+}

7e4335d2e10edb7fa2efed17411fcb6d972abaa1
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-11 23:28:12 +0530

    Rename class from Arrays to ArraysOne

---
+++ Day 10/ArraysOne.java
@@ -0,0 +1,14 @@
+public class ArraysOne {
+  public static void main(String[] args) {
+      int[] marks = {99,97,80};
+      
+
+      System.out.println(marks[0]);
+      System.out.println(marks[1]);
+      System.out.println(marks[2]);
+
+      for(int i=0 ; i<3 ; i++){
+      System.out.println(marks[i]);
+    }
+  }
+}

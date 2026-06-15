597eaef51e62708e2aaa688bc48a99a838402507
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-15 23:33:59 +0530

    Add PrintArrayNames.java to input and display names

---
+++ Day 12/PrintArrayNames.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class PrintArrayNames {
+  
+  public static void main(String[] args) {
+      
+    Scanner sc = new Scanner(System.in);
+
+    
+    System.out.print("Enter the number of names you want to input : ");
+    int size = sc.nextInt();
+
+    String names[] = new String[size];
+
+    System.out.println("Enter the names : ");
+    for(int i=0 ; i<size ; i++){
+      names[i] = sc.next();
+    }
+
+    System.out.println("Your names are : ");
+    for(int i=0 ; i<size ; i++){
+      System.out.println("Name at " + (i+1) + " is " + names[i]);
+    }
+
+  }
+}

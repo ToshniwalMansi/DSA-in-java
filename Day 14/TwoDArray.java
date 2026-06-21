5fc9c86f28b54e002b82a3c35f53a27da4ecb660
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-06-21 23:16:41 +0530

    Add TwoDArray class for 2D array input and output

---
+++ Day 14/TwoDArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class TwoDArray {
+  public static void main(String[] args) {
+      
+    Scanner sc = new Scanner(System.in);
+
+    int numbers[][] = new int[3][5];
+
+    for(int i=0 ; i<3 ; i++){
+      for(int j=0 ; j<5 ; j++){
+        numbers[i][j] = sc.nextInt();
+      }
+    }
+
+    for(int i=0 ; i<3 ; i++){
+      for(int j=0 ; j<5 ; j++){
+        System.out.print(numbers[i][j]);
+        System.out.print(" ");
+      }
+      System.out.println("");
+    }
+  }
+}

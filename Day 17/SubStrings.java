f406e4b633e45df7c5617d56517dd7add1578e01
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-07-02 18:38:36 +0530

    Modify substring logic to extract full name
    Updated substring extraction to get full name.

---
+++ Day 17/SubStrings.java
@@ -0,0 +1,32 @@
+public class SubStrings {
+  public static void main(String[] args) {
+      
+   /* String sentence = "My name is Mansi Toshniwal";
+
+    String name = sentence.substring(11,sentence.length());
+
+    System.out.println(name);
+
+    */
+
+
+
+    /*
+    
+    String sentence = "Mansi is my name ";
+
+    String name = sentence.substring(0,5);
+
+    System.out.println(name);
+
+    */
+
+    String sentence = "My name is Mansi Toshniwal";
+
+    String name = sentence.substring(11);
+
+    System.out.println(name);
+
+
+  }
+}

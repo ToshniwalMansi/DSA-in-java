7b142f330f61c28669da390b35c03962ad16b2ab
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-07-02 18:05:47 +0530

    Add Concatenation program to combine first and last names

---
+++ Day 17/Concatenation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Concatenation {
+  
+  public static void main(String[] args) {
+      
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter first name : ");
+    String firstName = sc.next();
+
+    System.out.print("Enter last name : ");
+    String lastName = sc.next();
+
+    String fullName = firstName +" "+ lastName ;
+
+    System.out.println("Your full name is : " + fullName);
+  }
+}

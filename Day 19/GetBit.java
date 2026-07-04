8dd9fa7ecc80e11d77afcc2d39294ea5b4e72a94
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-07-04 12:35:30 +0530

    Add GetBit class to check bit status

---
+++ Day 19/GetBit.java
@@ -0,0 +1,15 @@
+public class GetBit {
+  public static void main(String[] args) {
+      
+    int n = 5; //0101
+    int position = 2;
+    int bitMask = 1<<position ; 
+
+    if((bitMask & n) == 0){
+      System.out.println("Bit was zero ");
+    }
+    else{
+      System.out.println("Bit was one ");
+    }
+  }
+}

2b13d7b9fae4e7322c218898921e93c84a05edcd
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-07-04 11:46:21 +0530

    Add comment block for string replacement functionality
    Added a comment block explaining the purpose of the program.

---
+++ Day 19/ReplaceEandIStringBuilders.java
@@ -0,0 +1,27 @@
+/* 
+Input a string from the user. Create a new string called ‘result’ in which you will replace the letter ‘e’ in the original string with letter ‘i’. 
+Example : 
+original = “eabcdef’ ; result = “iabcdif”
+Original = “xyz” ; result = “xyz”
+*/
+
+
+import java.util.*;
+public class ReplaceEandIStringBuilders {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the word : ");
+    StringBuilder sb = new StringBuilder(sc.next());
+
+    for(int i=0 ; i<sb.length() ; i++){
+      if(sb.charAt(i) == 'e'){
+        sb.setCharAt(i,'i');
+      }
+    }
+
+    System.out.println("The new String is : " + sb);
+    sc.close();
+  }
+}

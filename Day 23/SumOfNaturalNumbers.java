dabf55c954392eda012f36490bc1ee18c1a351c6
Mansi Shirish Toshniwal <tmansi2107@gmail.com>
2026-07-08 20:33:03 +0530

    Add SumOfNaturalNumbers class to calculate sum

---
+++ Day 23/SumOfNaturalNumbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SumOfNaturalNumbers {
+  
+  public static void printSum(int i , int n , int sum){
+    if(i == n){
+      sum += i;
+      System.out.println("Sum is : " + sum);
+      return;
+    }
+
+    sum += i;
+    printSum(i+1 , n , sum);
+  }
+
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the number till you want sum : ");
+      int n = sc.nextInt();
+
+      printSum(1 , n ,0);
+      sc.close();
+  }
+}
